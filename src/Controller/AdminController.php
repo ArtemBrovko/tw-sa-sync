642c976f3ad3700a19e35b8de1349b6b696e9e59
Artem Brovko <artem.brovko@modera.net>
2019-03-12 14:03:18 +0200

    Change password functionality

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,8 +7,34 @@
 namespace App\Controller;
 
 use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
+use FOS\UserBundle\Model\UserManagerInterface;
 
 class AdminController extends EasyAdminController
 {
+    /**
+     * @var UserManagerInterface
+     */
+    private $userManager;
 
+    /**
+     * AdminController constructor.
+     *
+     * @param $userManager
+     */
+    public function __construct(UserManagerInterface $userManager)
+    {
+        $this->userManager = $userManager;
+    }
+
+
+    public function createNewUserEntity()
+    {
+        return $this->userManager->createUser();
+    }
+
+    public function updateUserEntity($user)
+    {
+        $this->userManager->updateUser($user, false);
+        parent::persistEntity($user);
+    }
 }
